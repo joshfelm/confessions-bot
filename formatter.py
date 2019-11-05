6052e87607229028b6854bbac460a1087e219659
fxlmo <josh.felmeden@outlook.com>
2019-11-05 12:33:26 +0000

    Fixed some issues with formatter

--- formatter.py
+++ formatter.py
@@ -12,7 +12,7 @@ for file in glob.glob("./gentext/gentext*.txt"):
         punctuation = ['.',')',',','!','?',':']
         capitals = ['.', '?', '!']
         post = ['(','"','\'', '*']
-        both= ['-','/','\\']
+        both= ['/','\\']
 
         if f.mode == 'r':
             fl = f.readlines()
@@ -21,26 +21,28 @@ for file in glob.glob("./gentext/gentext*.txt"):
                 i = 0
                 l = l[0].upper() + l[1:]
                 for c in l:
-                    if c == 'i':
-                        if l[i-1] == ' ' and l[i+1] == ' ':
-                            l = l[:i] + 'I' + l[i+1:]
-                    for p in punctuation:
-                        if p == c:
-                            l = l[:i-1] + l[i:]
-                            i-=1
-                    for p in post:
-                        if p == c:
-                            l = l[:i+1] + l[i+2:]
-                            i-=1
-                    for b in both:
-                        if b == c:
-                            l = l[:i-1] + l[i:]
-                            l = l[:i] + l[i+1:]
-                    for k in capitals:
-                        if k == c:
-                            if (i < (len(l) - 2)):
-                                if l[i+2].isalpha():
-                                    l = l[:i+2] + l[i+2].upper() + l[i+3:]
+                    if i > 0 and len(l) > 2:
+                        if c == 'i':
+                            if i < (len(l) - 1):
+                                if l[i-1] == ' ' and l[i+1] == ' ':
+                                    l = l[:i] + 'I' + l[i+1:]
+                        for p in punctuation:
+                            if p == c:
+                                l = l[:i-1] + l[i:]
+                                i-=1
+                        for p in post:
+                            if p == c:
+                                l = l[:i+1] + l[i+2:]
+                                i-=1
+                        for b in both:
+                            if b == c:
+                                l = l[:i-1] + l[i:]
+                                l = l[:i] + l[i+1:]
+                        for k in capitals:
+                            if k == c:
+                                if (i < (len(l) - 2)):
+                                    if l[i+2].isalpha():
+                                        l = l[:i+2] + l[i+2].upper() + l[i+3:]
                     i += 1
                 lines.append(l)
         f.close()
